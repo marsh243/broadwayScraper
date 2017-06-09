6a3e885eb612661ddd8a96296af45ed5dce2051c
marsh243 <marsh243@uwm.edu>
2017-06-08 23:03:17 -0500

    fixed bugs, ready to add dictionary of shows and their lottery info

--- broadwayScraper.py
+++ broadwayScraper.py
@@ -1,34 +1,35 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
-import urllib2
+import urllib3
+
+http = urllib3.PoolManager()
 
 url = "https://lottery.broadwaydirect.com/"  # initial site
 
-content = urllib2.urlopen(url).read()
+response = http.request('GET', url)
 
-soup = BeautifulSoup(content, "lxml")  # read the initial site in bs4
+soup = BeautifulSoup(response.data, "lxml")  # read the initial site in bs4
 
 d = {}  # dictionary for show name and url
 shows = {}  # will be show names + all relevant details
 info = {}
 for link in soup.find_all('a'):  # for every link find the shows specifically
     if "lottery.broadwaydirect.com/show/" in link.get('href'):
-        subContent = urllib2.urlopen(link.get('href')).read()
-        subSoup = BeautifulSoup(subContent, "lxml")  # subSoup checks each individual show
+        subResponse = http.request('GET', (link.get('href')))
+        subSoup = BeautifulSoup(subResponse.data, "lxml")  # subSoup checks each individual show
         title = subSoup.title.string.split(") ")  # trim the title and put in d and shows
-        print title[0] + ") " + link.get('href') + "\n"  # print for testing
+        # test = 1 if "CHICAGO" in title[0] else 0 an example on time differences for chicago showings
+        print(test, "\n")
+        print(title[0] + ") " + link.get('href') + "\n")  # print for testing
         d[title[0] + ")"] = link.get('href')  # attach title to url
-        table = subSoup.find('div',
-                                 class_="hide for tablets") # get table for info
-        rows = subSoup.table.find_all('div', class_="row lotteries-row hide-for-tablets show-for-desktop") #all the relevant data
-        print rows
+        rows = subSoup.find_all('div', class_="row lotteries-row hide-for-tablets show-for-desktop")
         for row in rows:
-            dateTime = subSoup.rows.find_all('div')[0].text # parse each column of a row (NOT WORKING)
-            status = subSoup.row.find_all('div')[1].text
-            tickets = subSoup.row.find_all('div')[2].text
-            price = subSoup.row.find_all('div')[3].text
-        print dateTime+" "+status+" "+tickets+" "+price
-        print "\n"
+            columns = row.find_all('div')
+            dateTime = [x for x in columns[0].text.split(' ') if x not in ['', '\n']]
+            status = [x for x in columns[1].text.split(' ') if x not in ['', '\n']]
+            tickets = [x for x in columns[2].text.split(' ') if x not in ['', '\n']]
+            price = [x for x in columns[3].text.split(' ') if x not in ['', '\n']]
+            print(dateTime, " ", status, " ", tickets, " ", price, "\n")
         # shows[title[0]+")"] =
 for a in d:  # print list of shows and urls
-    print a + " " + d[a] + "\n"
+    print(a, " ", d[a], "\n")
