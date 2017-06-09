f6312a9bbf232122f35bef1d2edaa10eec7e1bdb
marsh243 <marsh243@uwm.edu>
2017-06-08 21:05:10 -0500

    commented

--- broadwayScraper.py
+++ broadwayScraper.py
@@ -2,31 +2,28 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import urllib2
 
-url = "https://lottery.broadwaydirect.com/"
+url = "https://lottery.broadwaydirect.com/" #initial site
 
 content = urllib2.urlopen(url).read()
 
-soup = BeautifulSoup(content, "lxml")
+soup = BeautifulSoup(content, "lxml") #read the initial site in bs4
 
-d = {}
-shows = {}
-showTimes = {}
-parseInfo = {}
+d = {} #dictionary for show name and url
+shows = {} #will be show names + all relevant details
 
-for link in soup.find_all('a'):
+for link in soup.find_all('a'): #for every link find the shows specifically
     if "lottery.broadwaydirect.com/show/" in link.get('href'):
         subContent = urllib2.urlopen(link.get('href')).read()
-        subSoup = BeautifulSoup(subContent, "lxml")
-        title = subSoup.title.string.split(") ")
-        print title[0]+") "+link.get('href')+"\n"
-        d[title[0]+")"] = link.get('href')
-        table = subSoup.find_all('div', class_="row lotteries-row hide-for-tablets show-for-desktop")
+        subSoup = BeautifulSoup(subContent, "lxml") #subSoup checks each individual show
+        title = subSoup.title.string.split(") ") #trim the title and put in d and shows
+        print title[0]+") "+link.get('href')+"\n" #print for testing
+        d[title[0]+")"] = link.get('href') #attach title to url
+        table = subSoup.find_all('div', class_="row lotteries-row hide-for-tablets show-for-desktop") #find table of info
         for row in table:
-            info = row.text.split("\n")
+            info = row.text.split("\n") #split by line
         for a in info:
-            print a
-        #print dateTime
+            print a #print for testing (WONKY)
         print "\n"
         #shows[title[0]+")"] =
-for a in d:
+for a in d: #print list of shows and urls
     print a+" "+d[a]+"\n"
